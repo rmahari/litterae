ddf05f5c77407d18094afd5dadc72b6e56ed0e5a
Sarah <sshader@mit.edu>
2017-04-17 12:11:43 -0400

    Made "new annotation" dialog box that appears on double click

--- gr4/js/app.js
+++ gr4/js/app.js
@@ -1,6 +1,9 @@
 function LitteraeApp(el) {
     this.el = el;
     this.btn_marker = document.getElementById('btn-marker');
+    this.source_text = document.getElementById('text');
+    this.new_annotation = document.getElementById('new-annotation');
+    this.cancel_add = document.getElementById('cancel-add');
 
     this.bindEvents();
 }
@@ -10,4 +13,62 @@ LitteraeApp.prototype.bindEvents = function() {
     this.btn_marker.addEventListener('click', function(e) {
         self.btn_marker.classList.toggle('active');
     });
+
+    this.source_text.addEventListener('dblclick', function(e) {
+    		var new_annotation = document.getElementById('new-annotation');
+    		new_annotation.style.display = 'block';
+    		var welcome = document.getElementById('welcome');
+    		welcome.style.display = 'none';
+
+    		var selectedWord = (document.selection && document.selection.createRange().text) ||
+             (window.getSelection && window.getSelection().toString());
+
+      	var lineHeight = parseFloat(window.getComputedStyle(self.source_text, null).getPropertyValue('line-height'));
+  			var lineNumber = parseInt(e.offsetY/lineHeight) + 1;
+
+  			var lineNumField = document.getElementById('selected-line-num');
+  			var selectedField = document.getElementById('selected-text');
+  			lineNumField.innerHTML = lineNumber;
+  			selectedField.innerHTML  = selectedWord;
+    });
+
+    this.new_annotation.addEventListener('keyup', function(e) {
+    		var radios = document.querySelectorAll('input[type="radio"]:checked');
+				var value = radios.length;
+
+				var annotation = document.getElementById('annotation');
+				if (value === 2 && annotation.value.length > 0){
+					document.getElementById('save-add').disabled = false;
+				}
+				else {
+					document.getElementById('save-add').disabled = true;
+				}
+    });
+
+    this.new_annotation.addEventListener('click', function(e) {
+    		var radios = document.querySelectorAll('input[type="radio"]:checked');
+				var value = radios.length;
+
+				var annotation = document.getElementById('annotation');
+				if (value === 2 && annotation.value.length > 0){
+					document.getElementById('save-add').disabled = false;
+				}
+				else {
+					document.getElementById('save-add').disabled = true;
+				}
+    });
+
+    this.cancel_add.addEventListener('click', function(e) {
+    	  var new_annotation = document.getElementById('new-annotation');
+    		new_annotation.style.display = 'none';
+    		var welcome = document.getElementById('welcome');
+    		welcome.style.display = 'block';
+
+    		document.getElementById('annotation').value = '';
+    		var radios = document.querySelectorAll('input[type="radio"]:checked');
+    		for (var i = 0; i < radios.length; i++) {
+    			radios[i].checked = false;
+    		}
+    });
+
 }
