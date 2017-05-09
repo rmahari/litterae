09d9cc8fc46104a7bf7b35dddcbc183e1222fc4c
Lisandro J <lisandroj@dhcp-18-189-90-161.dyn.mit.edu>
2017-05-08 20:30:02 -0400

    add image upload and more canned data

--- gr4/js/canned.js
+++ gr4/js/canned.js
@@ -24,6 +24,7 @@ function getCannedAnnotations(me) {
     a3.author = me;
     a3.setText('lorem ipsum dolor sit amet lipsum quid est in nostrem hallos intra nos muros');
     a3.setCategory(2);
+    a3.setImage("https://cdn.thinglink.me/api/image/743685285547606017/1024/10/scaletowidth");
 
     var h4 = new Highlight();
     h4.addRange(30,33);
@@ -46,5 +47,35 @@ function getCannedAnnotations(me) {
     a6.setText('lipsum quid est in nostrem roto non est persona non grata hallos intra nos muros');
     a6.setCategory(1);
 
-    return [a1,a2,a3,a4,a5,a6];   
+    var h7 = new Highlight();
+    h7.addRange(54,58);
+    var a7 = new Annotation(h7);
+    a7.author = me;
+    a7.setText('non grata hallos intra nos muros');
+    a7.setCategory(2);
+    a7.setImage("https://upload.wikimedia.org/wikipedia/commons/b/ba/Map_Gallia_Tribes_Towns.png");
+
+    var h8 = new Highlight();
+    h8.addRange(94,98);
+    var a8 = new Annotation(h8);
+    a8.author = me;
+    a8.setText('viv o tradio non est persona');
+    a8.setCategory(2);
+    a8.setImage("http://www.hellenism.net/images/ancient-greece.jpg");
+
+    var h9 = new Highlight();
+    h9.addRange(126,129);
+    var a9 = new Annotation(h9);
+    a9.author = me;
+    a9.setText('ist nos patribus non sic sit amet');
+    a9.setCategory(1);
+
+    var h10 = new Highlight();
+    h10.addRange(133,137);
+    var a10 = new Annotation(h10);
+    a10.author = me;
+    a10.setText('hallos intra nos muros');
+    a10.setCategory(0);
+
+    return [a1,a2,a3,a4,a5,a6,a7,a8,a9,a10];   
 }
