7df4ac2a593bf8a38d07a9be826791674696132a
Joren <jorenl@mit.edu>
2017-05-10 18:18:13 -0400

    added some env stuff

--- service/app.js
+++ service/app.js
@@ -4,6 +4,10 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var application_root = process.env.application_root || process.cwd();
+var port = process.env.port || 3000;
+
+
 app.use(express.static(path.resolve('../gr4')));
 app.get('/', function(req, res){res.sendFile(path.resolve('../gr4/index.htm'));});
 
@@ -32,6 +36,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
