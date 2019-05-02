f3d032f88f35632bb8202bdb47100b9404636e80
Joren Lauwers <jorenl@mit.edu>
2019-05-02 01:56:27 -0700

    simplify

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || process.env.WEBSITE_PORT || 3000;
 
 
 app.use(express.static(path.resolve('./gr4')));
-app.get('/', function(req, res){res.sendFile(path.resolve('./gr4/app.htm'));});
+app.get('/app', function(req, res){res.sendFile(path.resolve('./gr4/app.htm'));});
 
 
 http.listen(port, function(){
